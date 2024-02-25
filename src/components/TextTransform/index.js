718dd14dbc437566961cf9a999fae3e07c9bb04c
Anderson Almeida <andersonpa10pa@gmail.com>
2024-02-25 10:23:16 -0300

    Criação do componente de texto

---
+++ src/components/TextTransform/index.js
@@ -0,0 +1,9 @@
+export function TextTransform({ color = 'black'}) {
+  return (
+    <p style={
+      {textTransform:"uppercase", color: color}
+    }>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio laudantium perferendis commodi ipsum deleniti nobis, at corporis quam. Veniam, esse temporibus. Quod non rerum delectus, ullam aut pariatur quisquam saepe?
+    </p>
+  )
+}
